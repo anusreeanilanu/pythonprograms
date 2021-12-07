d6203c648564b6c312cd9d82404ed0591c5a97d4
anusreeanilanu <94076173+anusreeanilanu@users.noreply.github.com>
2021-12-07 22:21:08 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from graphics.graphics3D import cuboid1
+cuboid1 .cuboid()
+from graphics import circle1
+circle1.circle()
+from graphics import rectangle1
+rectangle1.rectangle()
+from graphics.graphics3D import sphere1
+sphere1.sphere()
---
+++ graphics/graphics3D/sphere1.py
@@ -0,0 +1,9 @@
+import math
+def sphere():
+    radius=int(input("enter the radius of a sphere"))
+    area = 4*math.pi*radius*radius
+    print("area=",area)
+    pe= 2*math.pi*radius
+    print("perimeter=",pe)
+    
+    
